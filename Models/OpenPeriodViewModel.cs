c8e583c70b6a1287c66afa15f7da3d82b3c83f53
Obscuron <ndopoer@gmail.com>
2026-07-25 18:52:19 +0700

    Update OpenPeriodViewModel.cs

--- Models/OpenPeriodViewModel.cs
+++ Models/OpenPeriodViewModel.cs
@@ -1,1 +1,38 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace AumoFinance.Models
+{
+    public class OpenPeriodViewModel
+    {
+        [Required]
+        [Display(Name = "Month")]
+        public int Month { get; set; }
+
+        [Required]
+        [Display(Name = "Year")]
+        public int Year { get; set; }
+
+        [Required]
+        [Display(Name = "Cash Account")]
+        public int CashAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "Cash Opening Balance")]
+        [Range(0, double.MaxValue, ErrorMessage = "Balance cannot be negative.")]
+        public decimal CashBalance { get; set; }
+
+        [Required]
+        [Display(Name = "Bank Account")]
+        public int BankAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "Bank Opening Balance")]
+        [Range(0, double.MaxValue, ErrorMessage = "Balance cannot be negative.")]
+        public decimal BankBalance { get; set; }
+
+        [Required]
+        [Display(Name = "Retained Earnings Account")]
+        public int RetainedEarningsAccountId { get; set; }
+
+    }
+}
