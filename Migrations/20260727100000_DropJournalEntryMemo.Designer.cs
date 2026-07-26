61ab635c3ac6972ffd690323cd5a04645a3fc27f
Obscuron <ndopoer@gmail.com>
2026-07-27 05:32:45 +0700

    Add files via upload

--- Migrations/20260727100000_DropJournalEntryMemo.Designer.cs
+++ Migrations/20260727100000_DropJournalEntryMemo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AumoFinance.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
