05601d1a2e6315b8de693b78b7dce936d4996026
Obscuron <ndopoer@gmail.com>
2026-07-28 15:35:48 +0700

    Update AppDbContext.cs

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -6,21 +6,45 @@ namespace AumoFinance.Models
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) 
+        {
+        }
 
+        // Accounting
         public DbSet<ChartOfAccount> ChartOfAccounts => Set<ChartOfAccount>();
         public DbSet<JournalEntry> JournalEntries => Set<JournalEntry>();
         public DbSet<JournalEntryLine> JournalEntryLines => Set<JournalEntryLine>();
         public DbSet<Period> Periods { get; set; }
 
+
+        // Guardian Security
+        public DbSet<UserSession> UserSessions => Set<UserSession>();
+        public DbSet<TrustedDevice> TrustedDevices => Set<TrustedDevice>();
+        public DbSet<LoginActivity> LoginActivities => Set<LoginActivity>();
+        public DbSet<RecoveryCode> RecoveryCodes => Set<RecoveryCode>();
+        public DbSet<SecuritySetting> SecuritySettings => Set<SecuritySetting>();
+
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+
+            // ==========================================
+            // Chart Of Accounts
+            // ==========================================
+
             builder.Entity<ChartOfAccount>(entity =>
             {
-                entity.HasIndex(a => a.ReferenceNumber).IsUnique();
+                entity.HasIndex(a => a.ReferenceNumber)
+                      .IsUnique();
             });
+
+
+            // ==========================================
+            // Journal Entry
+            // ==========================================
 
             builder.Entity<JournalEntry>(entity =>
             {
@@ -30,15 +54,110 @@ namespace AumoFinance.Models
                       .OnDelete(DeleteBehavior.Cascade);
             });
 
+
             builder.Entity<JournalEntryLine>(entity =>
             {
-                // Setiap baris jurnal WAJIB merujuk ke satu akun di Chart of
-                // Account. Relasi ini yang membuat nomor referensi COA bisa
-                // "otomatis muncul" di General Journal maupun General Ledger.
                 entity.HasOne(l => l.Account)
                       .WithMany()
                       .HasForeignKey(l => l.AccountId)
                       .OnDelete(DeleteBehavior.Restrict);
+            });
+
+
+            // ==========================================
+            // Guardian - User Session
+            // ==========================================
+
+            builder.Entity<UserSession>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasIndex(x => x.UserId);
+
+                entity.HasIndex(x => x.RefreshTokenHash)
+                      .IsUnique();
+
+                entity.Property(x => x.DeviceName)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.Browser)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.IpAddress)
+                      .HasMaxLength(50);
+            });
+
+
+            // ==========================================
+            // Guardian - Trusted Device
+            // ==========================================
+
+            builder.Entity<TrustedDevice>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasIndex(x => x.UserId);
+
+                entity.HasIndex(x => x.DeviceIdentifier)
+                      .IsUnique();
+
+                entity.Property(x => x.DeviceName)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.Browser)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.OperatingSystem)
+                      .HasMaxLength(100);
+            });
+
+
+            // ==========================================
+            // Guardian - Login Activity
+            // ==========================================
+
+            builder.Entity<LoginActivity>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasIndex(x => x.UserId);
+
+                entity.Property(x => x.ActivityType)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.Device)
+                      .HasMaxLength(100);
+
+                entity.Property(x => x.IpAddress)
+                      .HasMaxLength(50);
+            });
+
+
+            // ==========================================
+            // Guardian - Recovery Code
+            // ==========================================
+
+            builder.Entity<RecoveryCode>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasIndex(x => x.UserId);
+
+                entity.Property(x => x.CodeHash)
+                      .HasMaxLength(255);
+            });
+
+
+            // ==========================================
+            // Guardian - Security Setting
+            // ==========================================
+
+            builder.Entity<SecuritySetting>(entity =>
+            {
+                entity.HasKey(x => x.UserId);
+
+                entity.Property(x => x.SessionTimeoutMinutes)
+                      .HasDefaultValue(30);
             });
         }
     }
