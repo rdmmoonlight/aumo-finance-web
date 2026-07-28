097e85a020c0451b66ab5e402f2ba23415e06850
Obscuron <ndopoer@gmail.com>
2026-07-28 15:34:38 +0700

    Create SecuritySetting.cs

---
+++ Models/Security/SecuritySetting.cs
@@ -0,0 +1,16 @@
+namespace AumoFinance.Models.Security;
+
+public class SecuritySetting
+{
+    public Guid UserId { get; set; }
+
+    public bool EmailVerified { get; set; }
+
+    public bool TwoFactorEnabled { get; set; }
+
+    public bool LoginNotificationEnabled { get; set; }
+
+    public int SessionTimeoutMinutes { get; set; } = 30;
+
+    public DateTime UpdatedAt { get; set; }
+}
