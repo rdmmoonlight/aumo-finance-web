f5b1a6ba762cb5159f51de2f7c40d6d7cb162520
Obscuron <ndopoer@gmail.com>
2026-08-04 13:54:45 +0700

    Remove unused ViewModels namespace from DocumentController

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AumoFinance.Models;
-using AumoFinance.ViewModels;
 using AumoFinance.Services; // Tambahkan namespace service Cloudinary kamu
 
 namespace AumoFinance.Controllers;
