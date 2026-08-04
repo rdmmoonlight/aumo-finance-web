99d43772a0f5965accf698e8bc077e067043c7bb
Obscuron <ndopoer@gmail.com>
2026-08-04 15:50:47 +0700

    Refactor TopBarView into MobileJournalEntryLine model

--- Models/MobileJournalEntryLine.cs
+++ Models/MobileJournalEntryLine.cs
@@ -1,58 +1,16 @@
-using Microsoft.Maui.ApplicationModel;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
-
-namespace AumoFinance.Views;
-
-public partial class TopBarView : ContentView
+namespace AumoFinance.Models
 {
-    public TopBarView()
+    public class MobileJournalEntryLine
     {
-        InitializeComponent();
-    }
+        public int Id { get; set; }
+        public int MobileJournalEntryId { get; set; }
+        public int AccountId { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public string? Description { get; set; }
 
-    #region Bindable Properties
-
-    public static readonly BindableProperty PeriodTextProperty =
-        BindableProperty.Create(
-            nameof(PeriodText),
-            typeof(string),
-            typeof(TopBarView),
-            default(string),
-            propertyChanged: OnPeriodTextChanged);
-
-    public string PeriodText
-    {
-        get => (string)GetValue(PeriodTextProperty);
-        set => SetValue(PeriodTextProperty, value);
-    }
-
-    private static void OnPeriodTextChanged(BindableObject bindable, object oldValue, object newValue)
-    {
-        if (bindable is TopBarView control && control.PeriodLabel != null)
-        {
-            control.PeriodLabel.Text = newValue as string;
-        }
-    }
-
-    #endregion
-
-    /// <summary>
-    /// Mengubah indikator badge sync (Hijau untuk terhubung/sukses, Merah untuk gagal/terputus)
-    /// </summary>
-    /// <param name="isSuccess">Status koneksi/sinkronisasi</param>
-    public void SetSyncStatus(bool isSuccess)
-    {
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            if (SyncBadge == null) return;
-
-            var statusColor = isSuccess
-                ? Color.FromArgb("#10B981")  // Emerald Green
-                : Color.FromArgb("#EF4444"); // Red
-
-            SyncBadge.BackgroundColor = statusColor;
-            SyncBadge.Stroke = statusColor;
-        });
+        // Navigation Properties untuk Entity Framework
+        public MobileJournalEntry? MobileJournalEntry { get; set; }
+        public ChartOfAccount? Account { get; set; }
     }
 }
