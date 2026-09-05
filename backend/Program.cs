e2e764ed35b8018f585257b046f7eeb2a23650aa
Obscuron <ndopoer@gmail.com>
2026-09-05 10:30:04 +0700

    Refactor AppDbContext registration in Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
+// Single File-Scoped Namespace (Wajib diletakkan tepat setelah using)
 namespace AumoBackend;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,7 +35,7 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.ConfigureWarnings(w => w.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
 });
 
-builder.Services.AddScoped<AppDbContext>(sp =>
+builder.Services.AddScoped<AppDbContext>(sp => 
     sp.GetRequiredService<IDbContextFactory<AppDbContext>>().CreateDbContext());
 
 // =====================================
