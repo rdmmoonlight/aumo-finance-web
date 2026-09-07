b5de1c9b8c61c6be92e6115aa8c2c9ef2ce8f94e
Obscuron <ndopoer@gmail.com>
2026-09-07 10:47:03 +0700

    Remove IJournalImportService registration

--- backend/Program.cs
+++ backend/Program.cs
@@ -226,7 +226,6 @@ namespace AumoBackend
 
             builder.Services.AddScoped<IGuardianService, GuardianService>();
             builder.Services.AddHttpClient<IAiService, AiService>();
-            builder.Services.AddScoped<IJournalImportService, JournalImportService>();
             builder.Services.AddScoped<ITransactionNumberService, TransactionNumberService>();
             builder.Services.AddMemoryCache();
             builder.Services.AddScoped<ICloudStorageService, CloudinaryService>();
