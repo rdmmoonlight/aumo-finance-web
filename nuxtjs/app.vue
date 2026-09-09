2bb9bff9aaf61e899448f2d5f8e8acc8ea97f1da
Obscuron <ndopoer@gmail.com>
2026-09-09 10:12:47 +0700

    edit dan tambah nuxtjs

---
+++ nuxtjs/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen overflow-hidden">
+    <!-- Component Sidebar -->
+    <Sidebar />
+
+    <div class="flex flex-col flex-1 overflow-y-auto">
+      <!-- Component Topbar -->
+      <Topbar />
+
+      <!-- Area konten dinamis halaman (seperti {children} di React) -->
+      <main class="p-4">
+        <NuxtPage />
+      </main>
+    </div>
+  </div>
+</template>
