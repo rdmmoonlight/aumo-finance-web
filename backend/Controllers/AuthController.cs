9e6e8e27f71f5b7c5d44822192a9f666185b2ec9
Obscuron <ndopoer@gmail.com>
2026-09-09 08:53:53 +0700

    Refactor namespace in AuthController

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace AumoBackend.Controllers.Web;
+namespace AumoBackend.Controllers;
 
 [ApiController]
 [Route("/api/v1/auth")]
