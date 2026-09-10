e2db10023ae5982dab5ef403c331b43211543093
Obscuron <ndopoer@gmail.com>
2026-09-10 21:09:52 +0700

    Change API base URL to WEB_API_BASE_URL

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { API_BASE_URL } from '@/lib/config';
+import { WEB_API_BASE_URL } from '@/lib/config';
 const apiClient = axios.create({
-  baseURL: API_BASE_URL, // https://aumonext-api.onrender.com
+  baseURL: WEB_API_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 apiClient.interceptors.request.use(c => {
