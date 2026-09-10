df7ac20f4a92159e9ccbe2232d923ea78f731218
Obscuron <ndopoer@gmail.com>
2026-09-10 20:32:00 +0700

    Remove prefixes from envPrefix in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'API_'],
+  envPrefix: [''],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   build: { cssMinify: false },
 })
