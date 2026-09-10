810e611dabdff943d977e296206bef0f77a47de4
Obscuron <ndopoer@gmail.com>
2026-09-10 10:02:16 +0700

    Clean up CSS imports in main.tsx
    Removed unused CSS imports from main.tsx.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-
-// Import global styles
 import './styles/globals.css';
-import './styles/index.css';
-import './styles/utilities.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
