24521a4e38f08d4bcd7293b33bff07cddf8303a0
Obscuron <ndopoer@gmail.com>
2026-09-11 14:24:43 +0700

    Add dark class to html element in RootDocument

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -26,7 +26,7 @@ function RootComponent() {
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <HeadContent />
       </head>
